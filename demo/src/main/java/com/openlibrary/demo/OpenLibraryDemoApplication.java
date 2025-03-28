4cdf07ea3cfc7d84216fa7367dc6402a08d5fd21
linnotendal <linn.otendal@gmail.com>
2025-03-28 11:37:27 +0100

    förra koden

---
+++ demo/src/main/java/com/openlibrary/demo/OpenLibraryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.openlibrary.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenLibraryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenLibraryDemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/openlibrary/demo/OpenLibraryDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openlibrary.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenLibraryDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
