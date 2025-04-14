5209f057c7fc0158b66cc5f3e090793d6e5d64cf
Delaram Azad <aramazad@live.se>
2025-04-14 13:53:47 +0200

    a separete file for JavaScript, fixed the buttons for login/signup page

---
+++ demo/src/main/resources/static/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const sidebar = document.getElementById('sidebar');
+
+    menuToggle.addEventListener('click', function() {
+        sidebar.classList.toggle('sidebar-hidden');
+        menuToggle.classList.toggle('active');
+    });
+});
