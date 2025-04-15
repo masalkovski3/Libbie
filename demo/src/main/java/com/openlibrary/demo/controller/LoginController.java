70de5759986144ad74fb8908414766dee5761618
Emmi Masalkovski <emmi@dmon.se>
2025-04-15 12:21:54 +0200

    Tagit bort onödig kod

--- demo/src/main/java/com/openlibrary/demo/controller/LoginController.java
+++ demo/src/main/java/com/openlibrary/demo/controller/LoginController.java
@@ -7,64 +7,10 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class LoginController {
 
-   // private final MemberRepository memberRepository;
-
-   /* public LoginController(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    */
-
-
-
     @GetMapping("/logIn")
     public String logIn() {
         return "logIn";
     }
-
-    /*
-    @PostMapping("/signup")
-    public String signup(@RequestParam String firstname,
-                         @RequestParam String email,
-                         @RequestParam String password,
-                         RedirectAttributes redirectAttributes) {
-
-        if (memberRepository.findByEmail(email).isPresent()) {
-            redirectAttributes.addFlashAttribute("error", "Email already in use.");
-            return "/signup";
-        }
-
-        Member newMember = new Member(firstname, email, password); // Obs: lösenord i klartext, fixas senare
-        memberRepository.save(newMember);
-
-        redirectAttributes.addFlashAttribute("message", "Signup successful! You can now log in.");
-        return "/signup";
-    }
-
-    @PostMapping("/login")
-    public String login(@RequestParam String email,
-                        @RequestParam String password,
-                        HttpSession session,
-                        RedirectAttributes redirectAttributes) {
-
-        Optional<Member> memberOpt = memberRepository.findByEmail(email);
-
-        if (memberOpt.isPresent() && memberOpt.get().getPassword().equals(password)) {
-            session.setAttribute("loggedInMember", memberOpt.get());
-            return "redirect:/profile";
-        }
-
-        redirectAttributes.addFlashAttribute("error", "Invalid email or password.");
-        return "redirect:/logIn";
-    }
-
-    @GetMapping("/logout")
-    public String logout(HttpSession session) {
-        session.invalidate();
-        return "redirect:/";
-    }
-
-     */
 
     @PostMapping("/logIn")
     @ResponseBody
@@ -73,5 +19,4 @@ public class LoginController {
         System.out.println("password = " + password);
         return "OK";
     }
-
 }
