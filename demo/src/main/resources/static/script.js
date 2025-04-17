9d2511cef4fe38d8ff3be34b779428850543664b
Amelie Music <amelielisaa@gmail.com>
2025-04-17 11:36:17 +0200

    Forts. på färgskala och design
    Forts. på färgskala och design i search-page.

--- demo/src/main/resources/static/script.js
+++ demo/src/main/resources/static/script.js
@@ -7,3 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
         menuToggle.classList.toggle('active');
     });
 });
+
+//Javascript för att slidern med årtalen i advanced filters ska uppdateras.
+const slider = document.getElementById('yearFilter');
+const yearDisplay = document.getElementById('yearValue');
+
+if (slider && yearDisplay) {
+    yearDisplay.textContent = slider.value;
+
+    slider.addEventListener('input', () => {
+        yearDisplay.textContent = slider.value;
+    });
+}
